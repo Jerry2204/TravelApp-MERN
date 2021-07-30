8699876e949bcd8246901e8fef458ad29464be3b
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-07-30 22:02:01 +0700

    finishing inputNumber

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
+import ExamplePage from "pages/Example";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        {/* <Route path="/" component={LandingPage}></Route> */}
+        <Route path="/example" component={ExamplePage}></Route>
       </Router>
     </div>
   );
