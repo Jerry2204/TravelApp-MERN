0f11ca0ae61d7fb8fe8da38cdb7d8a3ce80afbbd
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-08-23 14:41:29 +0700

    page detail part 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
 import ExamplePage from "pages/Example";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Route path="/" component={LandingPage}></Route> */}
-        <Route path="/example" component={ExamplePage}></Route>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/example" component={ExamplePage} />
       </Router>
     </div>
   );
