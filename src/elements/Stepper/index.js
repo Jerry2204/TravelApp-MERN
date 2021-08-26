132bed3e89423e9f233850918b5e1eb82fb95e24
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-08-26 14:17:10 +0700

    stepper components

---
+++ src/elements/Stepper/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import propTypes from "prop-types";
+
+export default function Stepper(props) {
+  const { steps, initialStep } = props;
+  const stepsKeys = Object.keys(steps);
+
+  const [currentStep, setCurrenStep] = useState(
+    stepsKeys.indexOf(initialStep) > -1 ? initialStep : stepsKeys[0]
+  );
+
+  const totalStep = stepsKeys.length;
+  const indexStep = stepsKeys.indexOf(currentStep);
+
+  function prevStep() {
+    if (+indexStep > 0) setCurrenStep(stepsKeys[indexStep - 1]);
+  }
+
+  function nextStep() {
+    if (+indexStep < totalStep) setCurrenStep(stepsKeys[indexStep + 1]);
+  }
+
+  return <>{props.children(prevStep, nextStep, currentStep, steps)}</>;
+}
+
+Stepper.propTypes = {
+  data: propTypes.object.isRequired,
+  initialStep: propTypes.string,
+};
--- src/parts/Header.js
+++ src/parts/Header.js
@@ -14,7 +14,9 @@ export default function Header(props) {
         <header className="spacing-sm">
           <div className="container">
             <nav className="navbar navbar-expand-lg navbar-light">
-              <Button className="text-gray-900">cation.</Button>
+              <Button className="brand-text-icon mx-auto" href="" type="link">
+                Stay<span className="text-gray-900">cation.</span>
+              </Button>
             </nav>
           </div>
         </header>
