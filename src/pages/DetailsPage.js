bfb412c0ca9646821c70dcc29c31ed6fe058da28
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-06-19 23:51:30 +0700

    finishing integration detail page

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,61 +1,82 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
 import { connect } from 'react-redux';
-import Header from 'parts/Header';
 
+import Header from 'parts/Header';
 import PageDetailTitle from 'parts/PageDetailTitle';
-import ItemDetails from '../json/itemDetails.json';
 import FeaturedImage from 'parts/FeaturedImage';
 import PageDetailDesc from 'parts/PageDetailDesc';
 import BookingForm from 'parts/BookingForm';
 import Categories from 'parts/Categories';
 import Testimoni from 'parts/Testimoni';
 import Footer from 'parts/Footer';
-import Fade from 'react-reveal/Fade';
 
 import { checkoutBooking } from 'store/actions/checkout';
+import { fetchPage } from 'store/actions/page';
+import Activities from 'parts/Activities';
 
 class DetailsPage extends Component {
   componentDidMount() {
     window.title = 'Details Page';
     window.scrollTo(0, 0);
+
+    console.log('Mulai test');
+    if (!this.props.page[this.props.match.params.id])
+      this.props.fetchPage(
+        `${process.env.REACT_APP_HOST}/api/detail-page/${this.props.match.params.id}`,
+        this.props.match.params.id
+      );
+
+    console.log('Akhir test');
   }
 
   render() {
+    const { page, match } = this.props;
+
+    if (!page[match.params.id]) return null;
+
     const breadcrumb = [
       { pageTitle: 'Home', pageHref: '' },
       { pageTitle: 'House Details', pageHref: '' },
     ];
+
     return (
       <>
         <Header {...this.props} />
         <PageDetailTitle
           breadcrumb={breadcrumb}
-          data={ItemDetails}
+          data={page[match.params.id]}
         ></PageDetailTitle>
-        <FeaturedImage data={ItemDetails.imageUrls} />
+        <FeaturedImage data={page[match.params.id].imageId} />
         <section className="container">
           <div className="row">
             <div className="col-7 pr-5">
               <Fade bottom>
-                <PageDetailDesc data={ItemDetails} />
+                <PageDetailDesc data={page[match.params.id]} />
               </Fade>
             </div>
             <div className="col-5">
               <Fade bottom>
                 <BookingForm
-                  itemDetails={ItemDetails}
+                  itemDetails={page[match.params.id]}
                   startBooking={this.props.checkoutBooking}
                 />
               </Fade>
             </div>
           </div>
         </section>
-        <Categories data={ItemDetails.categories} />
-        <Testimoni data={ItemDetails.testimonial} />
+        <Activities data={page[match.params.id].activityId} />
+        <Testimoni data={page[match.params.id].testimonial} />
         <Footer />
       </>
     );
   }
 }
 
-export default connect(null, { checkoutBooking })(DetailsPage);
+const mapStateToProps = (state) => ({
+  page: state.page,
+});
+
+export default connect(mapStateToProps, { checkoutBooking, fetchPage })(
+  DetailsPage
+);
--- src/parts/PageDetailDesc.js
+++ src/parts/PageDetailDesc.js
@@ -1,5 +1,5 @@
-import React from "react";
-import parse from "html-react-parser";
+import React from 'react';
+import parse from 'html-react-parser';
 
 export default function PageDetailDesc({ data }) {
   return (
@@ -7,26 +7,28 @@ export default function PageDetailDesc({ data }) {
       <h4>About The Place</h4>
       {parse(data.description)}
       <div className="row" style={{ marginTop: 30 }}>
-        {data.features.map((feature, index) => {
-          return (
-            <div
-              className="col-3"
-              key={`featured-${index}`}
-              style={{ marginBottom: 20 }}
-            >
-              <img
-                src={feature.imageUrl}
-                alt={feature.name}
-                width="38"
-                className="d-block mb-2"
-              />
-              <span>{feature.qty} </span>
-              <span className="text-gray-500 font-wight-light">
-                {feature.name}
-              </span>
-            </div>
-          );
-        })}
+        {data.featureId.length === 0
+          ? 'No feature'
+          : data.featureId.map((feature, index) => {
+              return (
+                <div
+                  className="col-3"
+                  key={`featured-${index}`}
+                  style={{ marginBottom: 20 }}
+                >
+                  <img
+                    src={`${process.env.REACT_APP_HOST}/${feature.imageUrl}`}
+                    alt={feature.name}
+                    width="38"
+                    className="d-block mb-2"
+                  />
+                  <span>{feature.qty} </span>
+                  <span className="text-gray-500 font-wight-light">
+                    {feature.name}
+                  </span>
+                </div>
+              );
+            })}
       </div>
     </main>
   );
